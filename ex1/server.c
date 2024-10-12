5360ef3c324269d7c5678d4a75dd42ac0c2fbcca
sgtjlanc <jamowaind@gmail.com>
2024-10-12 17:50:23 +0100

    exercise 1

---
+++ ex1/server.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int main (int argc, char ** argv) {
+    /* to be written */
+    while (true) {
+        printf("Server to be written\n");
+        return 0;
+    }
+    
+}
