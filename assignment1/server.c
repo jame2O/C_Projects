a486190938e5ca88b4a2e70278fe00b6f80e2042
james2O <jamowaind@gmail.com>
2024-10-16 12:59:12 +0100

    Commit to laptop

--- assignment1/server.c
+++ assignment1/server.c
@@ -3,11 +3,14 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-void printRequests(char* arg, int pos) {
+void printRequests(char* reqs, int pos) {
     //Loop through the request array, printing each one
     for (int i=0; i<pos; i++) {
-        printf("%c\n", arg[i]);
+        printf("%c\n", reqs[i]);
     }
+}
+void addRequests(char* reqs, int nReq) {
+
 }
 
 int main (int argc, char ** argv) {
@@ -17,22 +20,29 @@ int main (int argc, char ** argv) {
     reqs = (char*)malloc(capacity*sizeof(char));
     
     while (true) {
+
         //Checks if we need to add more memory to the request list.
         if (reqPos >= capacity) {
             capacity *= 2;
             reqs = (char*)realloc(reqs, capacity*sizeof(char));
         }
+
         //Accept next Request, add to list
         char nextReq;
         printf("\n- Please enter a request: ");
         scanf(" %c", &nextReq);
         reqs[reqPos] = nextReq;
         reqPos++;
+
         //Process the request:
         switch (nextReq) {
             case 'R':
                 printRequests(reqs, reqPos);
                 break;
+            case 'A':
+                addRequest();
+                break;
+            case 'C';
         }
     }
     free(reqs);
