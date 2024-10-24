edda818011681a418c776c9e4599c9bc7d132e3c
james2O <jamowaind@gmail.com>
2024-10-24 09:29:42 +0100

    To Laptop

--- assignment1/server.c
+++ assignment1/server.c
@@ -1,21 +1,23 @@
 // NOTE: WINDOWS DECS ONLY. GET RID IF TESTING ON LINUX!
 #define _GNU_SOURCE
 #include <stdio.h>
-//#include <pthread.h>
+#include <pthread.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Rule {
     char ip[2][8];
     int ports[2];
 } Rule;
+
 void printRequests(char* reqs, int pos) {
     //Loop through the request array, printing each one
     for (int i=0; i<pos; i++) {
         printf("%c\n", reqs[i]);
     }
 }
-void addRule(int pos, int cap, Rule* rules) {
+int addRule(int pos, int cap, Rule* rules) {
     // Checks if we need to add more memory to the rules list.
     if (pos >= cap)
     {
@@ -24,36 +26,61 @@ void addRule(int pos, int cap, Rule* rules) {
     }
 
     //Accept rule input
-    //Rule newRule;
-    char *inpBuf;
-    size_t bufSize = 100;
+    char *inpBuf = NULL;
+    size_t bufSize = 0;
     size_t inpChars;
-    //char ip[];
-    //char port[];
 
     inpBuf = (char*)malloc(bufSize * sizeof(char));
+    inpChars = 0;
 
     printf("\n- Please enter a new rule in the form <IPAddress> <ports>: ");
+    inpChars = getline(&inpBuf, &bufSize, stdin);
     
-    inpChars = 0;
-    //Get input and check str length
-    while (!inpChars == ) {
-        inpChars = getline(&inpBuf, &bufSize, stdin);
-        if (!inpChars == 37 && !inpChars == 18) {
-            printf("\n- Rule invalid. Please try again");
-        } else {
-            break;
+    //IP Checks
+    char *end = inpBuf;
+    char *start = inpBuf;
+    //Split IP & Port
+    while (*end != ' ' && *end != '\0') {
+        end++;
+    }
+    char* ip = (char*)malloc((end-start)+1)
+    strncpy(ip, start, (end-start));
+    start = end;
+    while (*end != '\0') {
+        end++;
+    }
+    char* port = (char*)malloc((end-start)+1)
+    strncpy(port, start, (end-start));
+
+    //IP Validation
+    start = ip;
+    end = ip;
+    while (*end != '\0') {
+        while (*end != '-' && *end != '\0') {
+            end++;
         }
-    } 
-    
-    //Check if it's valid
-    printf("\n- You inputted %s", inpBuf);
+        int ipLen = end - start;
+        if (ipLen != 15) { //Length Check
+            printf("\nInvalid rule");
+            return 0;
+        }
+        char* token = (char*)malloc(tokLen + 1);
+        strncpy(token, start, tokLen);
+
+        if ((atoi(token) == 0) || (atoi(token) < 0) || (atoi(token) > 255)) { //Num check
+            printf("\nInvalid rule");
+            return 0;
+        }
+        
+        free(token);
+    }
     //Add to the rules list
 
     //Print response
-
-    // Free buffer
+    
+    // Free mallocs & return
     free(inpBuf);
+    return 0;
 }
 
 
@@ -79,7 +106,9 @@ int main (int argc, char ** argv) {
         //Accept next Request, add to list
         char nextReq;
         printf("\n- Please enter a request: ");
-        scanf(" %c", &nextReq);
+        scanf("%c", &nextReq);
+        int ch;
+        while ((ch = getchar()) != '\n' && ch != EOF); 
         reqs[reqPos] = nextReq;
         reqPos++;
 
